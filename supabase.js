3cd43348a1a8141d837ab5885971c95f4222bbf5
Nevachno <mrsaves868@gmail.com>
2026-07-27 19:26:11 +0500

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,17 +1,19 @@
 const SUPABASE_URL =
     "https://oudxsihtfvfhyglqqarq.supabase.co/rest/v1/";
 
+
 const SUPABASE_PUBLISHABLE_KEY =
     "sb_publishable_7jN-tI3jAfuendFXY61JJg_B6fvk5Zi";
 
 
-const supabaseClient =
+window.supabaseClient =
     window.supabase.createClient(
         SUPABASE_URL,
         SUPABASE_PUBLISHABLE_KEY
     );
 
-    console.log(
+
+console.log(
     "Supabase подключён:",
-    supabaseClient
+    window.supabaseClient
 );
